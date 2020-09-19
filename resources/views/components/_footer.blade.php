b7fef1c4eee02c348b4f3be30cff585d06ad1f1e
Putra Langkat <putralangkat97@gmail.com>
2020-09-19 12:11:22 +0700

    base template telah ditambahkan

--- resources/views/components/_footer.blade.php
+++ resources/views/components/_footer.blade.php
@@ -3,7 +3,7 @@
         <div class="row">
             <div class="col-md-12">
                 <div class="copyright">
-                    <p>Copyright © {{ date('Y') }} Putralangkat97. All rights reserved. Template by <a href="https://colorlib.com">Colorlib</a>.</p>
+                    <p>Copyright © {{ date('Y') }} <a target="_blank" href="https://putralangkat97.github.io/">Putralangkat97.</a> All rights reserved. Template by <a href="https://colorlib.com">Colorlib</a>.</p>
                 </div>
             </div>
         </div>
