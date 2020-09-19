116a502ee7d8c1ac7feb59ff83e0cadd8308ab18
Putra Langkat <putralangkat97@gmail.com>
2020-09-19 15:01:12 +0700

    menambahkan eloquent relasi tabel event dan user

--- app/Event.php
+++ app/Event.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Event extends Model
 {
     use SoftDeletes;
+
+    public function users() {
+        return $this->belongsToMany('App\User');
+    }
 }
