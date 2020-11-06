37fd3ff228f8efe084f11394f59fb67563e7f9f1
Putra Langkat 97 <pucukpisang97@gmail.com>
2020-11-06 13:30:43 +0700

    dikit2

--- public/js/app.js
+++ public/js/app.js
@@ -37331,8 +37331,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/anggit-ghibli/Laravel/ujian/mycbt/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/anggit-ghibli/Laravel/ujian/mycbt/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/anggitau2/Laravel/myproject/mycbt/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/anggitau2/Laravel/myproject/mycbt/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
